ab770add36818d1d64d9f3b2f6c850445e4f3cee
arthur <geleminecrafter@gmail.com>
2021-10-23 11:09:47 +0200

    Supporting help messages

--- ExampleProject/src/FrontendWorker.ts
+++ ExampleProject/src/FrontendWorker.ts
@@ -3,11 +3,20 @@
 import { sendData } from "./wsConnection";
 
 const outputPart = document.getElementById("output") as HTMLElement;
+const outputHelp = document.getElementById("outputHelp") as HTMLElement;
 const connectionType = document.getElementById("conType") as HTMLElement;
 
 export function AddData(data: string) {
   //   outputPart.innerText = insert(outputPart.innerText,0, data)
   outputPart.innerText = data;
+
+  let parsedData = JSON.parse(data);
+
+  // console.log(parsedData);
+
+  if(parsedData.type == "help"){
+    outputHelp.innerText = data;
+  }
 }
 
 export function ChangeConnectionType(con:string){
@@ -15,6 +24,7 @@ export function ChangeConnectionType(con:string){
 }
 
 const InputField = document.getElementById("inputField") as HTMLInputElement;
+// InputField.value = `{"Type":"Teleport","X":40,"Y":10,"Z":15,"RotX":0,"RotY":0,"RotZ":0}`;
 
 const SendButton = document.getElementById("send") as HTMLButtonElement;
 SendButton.addEventListener("click", () => {
