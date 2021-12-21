ae795d278704abd5405e080992e033671c99fa64
arthur <geleminecrafter@gmail.com>
2021-12-21 01:21:42 +0100

    Got it kinda working

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <template>
-  <player-tab />
-
+  <div class="container">
+    <div>
+      <player-tab
+        v-for="(item, index) in redTeamList"
+        :key="index"
+        :playerName="item.name"
+        :leftWeapon="item.leftWeapon"
+        :rightWeapon="item.rightWeapon"
+        :Dash="item.dash"
+        :Health="item.health"
+        :team="item.team"
+      />
+    </div>
+    <div></div>
+    <div class="floatleft">
+      <player-tab
+        v-for="(item, index) in blueTeamList"
+        :key="index"
+        :playerName="item.name"
+        :leftWeapon="item.leftWeapon"
+        :rightWeapon="item.rightWeapon"
+        :Dash="item.dash"
+        :Health="item.health"
+        :team="item.team"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { HOSt, PORT } from "./ConstVars";
 import PlayerTab from "./components/PlayerTab.vue";
+
+interface playerInfo {
+  name: string;
+  team: number;
+  leftWeapon: string;
+  rightWeapon: string;
+  health: number;
+  dash: number;
+}
 
 export default defineComponent({
   name: "App",
@@ -14,13 +49,98 @@ export default defineComponent({
   },
   data() {
     return {
-      title: "wow",
+      PlayerData: [] as playerInfo[],
+      connection: "Failed",
     };
+  },
+  computed: {
+    redTeamList() {
+      let data = this.PlayerData as playerInfo[];
+      return data.filter((e: playerInfo) => {
+        return e.team == 0;
+      });
+    },
+    blueTeamList() {
+      let data = this.PlayerData as playerInfo[];
+      return data.filter((e: playerInfo) => {
+        return e.team == 1;
+      });
+    },
+  },
+  methods: {
+    updateData(data: string) {
+      var socketData = JSON.parse(data);
+      switch (socketData.type) {
+        case "playerJoins":
+          console.log(socketData);
+          this.PlayerData.push({
+            name: socketData.name,
+            team: socketData.team,
+            leftWeapon: "pistol",
+            rightWeapon: "pistol",
+            health: 100,
+            dash: 100,
+          });
+
+          break;
+        case "playerLeaves":
+          console.log(socketData);
+          this.PlayerData.splice(socketData.spectatorIndex, 1);
+          break;
+        case "loadoutUpdate":
+          this.PlayerData[socketData.spectatorIndex].leftWeapon =
+            socketData.leftHand;
+          this.PlayerData[socketData.spectatorIndex].rightWeapon =
+            socketData.rightHand;
+          break;
+        default:
+          // console.log("Support " + socketData.type + " Please");
+          break;
+      }
+    },
+  },
+  mounted() {
+    let ws: WebSocket;
+
+    const StartWebSocket = () => {
+      if (ws != null) {
+        ws.removeEventListener("error", failed);
+        ws.removeEventListener("close", failed);
+        ws.removeEventListener("open", onConnected);
+        ws.removeEventListener("message", onMessage);
+      }
+
+      ws = new WebSocket(`ws://${HOSt}:${PORT}`);
+      this.connection = "Connecting";
+
+      ws.addEventListener("error", failed);
+      ws.addEventListener("close", failed);
+      ws.addEventListener("open", onConnected);
+      ws.addEventListener("message", onMessage);
+    };
+
+    document.body.onfocus = () => {
+      if (ws == null || ws.readyState != 1) StartWebSocket();
+    };
+
+    const failed = () => {
+      this.connection = "Failed. Enable websocket and restart HyperDash";
+    };
+
+    const onConnected = () => {
+      this.connection = "";
+    };
+
+    const onMessage = (ev: MessageEvent<string>) => {
+      this.updateData(ev.data);
+    };
+
+    StartWebSocket();
   },
 });
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: Arial;
 }
@@ -28,5 +148,15 @@ export default defineComponent({
 body {
   margin: 0px;
   padding: 0px;
+}
+
+.floatleft {
+  justify-self: end;
+}
+.container {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  // justify-items: self-end;
+  // gap: 10em 10em;
 }
 </style>
--- src/components/PlayerTab.vue
+++ src/components/PlayerTab.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="backdrop">
+  <div class="backdrop" :class="{ redTeam: team == 0, blueTeam: team == 1 }">
     <img src="@/assets/helmet1.png" alt="helmet1" class="icon" />
-    <div class="playerInfo">
-      <a>Name</a>
+
+    <div class="container">
+      <div class="Name">
+        <p>{{ playerName }}</p>
+      </div>
+      <div class="playerInfo">
+        <p>{{ leftWeapon }}</p>
+        <p>{{ rightWeapon }}</p>
+        <p>{{ health }}:Health</p>
+        <p>{{ dash }}:dash</p>
+      </div>
     </div>
   </div>
 </template>
@@ -12,21 +21,62 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "PlayerTab",
+  data() {
+    return {
+      // playerName: "testName",
+      // leftWeapon: "testPistol",
+      // rightWeapon: "testPistol",
+      // Health: 100,
+      // Dash: 100,
+    };
+  },
+  props: ["playerName", "leftWeapon", "rightWeapon", "health", "dash", "team"],
 });
 </script>
 
-<style>
+<style scoped>
 .icon {
   width: 100px;
   height: 100px;
   display: block;
 }
+
 .backdrop {
   width: 500px;
   background: rgba(0, 0, 0, 0.5);
-  display: flex;
+  display: grid;
+  grid-template-columns: auto 1fr;
 }
+
+.redTeam {
+  background: red;
+}
+.blueTeam {
+  background: blue;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: min-content 1fr;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    "Name"
+    "playerInfo";
+}
+
+.Name {
+  grid-area: Name;
+  justify-self: center;
+}
+
 .playerInfo {
-  width: 100px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+
+  grid-area: playerInfo;
+  align-items: end;
+  justify-items: center;
 }
 </style>
