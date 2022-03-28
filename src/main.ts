401082774cfa7f9424a6550434950bc8301ed20f
Arthur <geleminecrafter+git@gmail.com>
2022-03-28 23:26:31 +0200

    Removed volar

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store/store";
 
-import * as volar from "@volar/experimental/client";
 
 createApp(App)
-    .use(volar.vuePlugin)
     .use(store)
     .mount("#app");
