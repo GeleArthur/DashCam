d95cbc9165e30cfd6881dd39a742870a7c8bf1fd
Styledev <dewey.bushaw@parapxl.com>
2022-03-28 22:33:07 -0600

    dln - healthbars for players and selected players.

--- src/components/PlayerLayout.vue
+++ src/components/PlayerLayout.vue
@@ -1,7 +1,5 @@
 <template>
 	<div class="container">
-
-
 		<div class="team team--blue">
 			<div class="team__players team__players--blue">
 				<new-player-tab
@@ -44,53 +42,63 @@
 		
 		<div class="playerBar" :disabled="$store.state.selectedIndex == 0">
 			<div class="playerBar_wrapper">
-				<div class="playerBar__dashes">=====</div>
+				<div class="playerBar__dashes">{{selectedPlayer.dash.toFixed(1)}}</div>
 				<div class="playerBar__score">{{selectedPlayer.score}}</div>
-				<div class="playerBar__health">=====</div>
+				<div class="playerBar__health">
+					<div class="healthBar">
+						<div class="healthBar__life" :style="healthBar"></div>
+						<div class="healthBar__track"></div>
+					</div>
+				</div>
 				<div class="playerBar__kills">{{selectedPlayer.kills}}</div>
 				<div class="playerBar__name">{{selectedPlayer.name}}</div>
 				<div class="playerBar__deaths">{{selectedPlayer.deads}}</div>
 			</div>
 		</div>
-		
 	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { mapMutations, mapState } from "vuex";
-import playerInfo from "../models/playerInfo";
-import NewPlayerTab from "./NewPlayerTab.vue";
-import MiniMap from "./MiniMap.vue";
-
-export default defineComponent({
-		name:"PlayerLayout",
-		components: {
-		NewPlayerTab,
-		MiniMap,
-	},
-
-	computed: mapState({
-		redTeamList() {
-			let data = this.$store.state.PlayerData as playerInfo[];
-			return data.filter((e: playerInfo) => {
-				return e.team == 0;
-			});
+	import { defineComponent } from "vue";
+	import { mapMutations, mapState } from "vuex";
+	import playerInfo from "../models/playerInfo";
+	import NewPlayerTab from "./NewPlayerTab.vue";
+	import MiniMap from "./MiniMap.vue";
+	
+	export default defineComponent({
+			name:"PlayerLayout",
+			components: {
+			NewPlayerTab,
+			MiniMap,
 		},
-		blueTeamList() {
-			let data = this.$store.state.PlayerData as playerInfo[];
-			return data.filter((e: playerInfo) => {
-				return e.team == 1;
-			});
-		},
-		selectedPlayer() {
-			return this.$store.state.PlayerData[this.$store.state.selectedIndex];
-		},
-	}),
-});
+		computed: mapState({
+			redTeamList() {
+				let data = this.$store.state.PlayerData as playerInfo[];
+				return data.filter((e: playerInfo) => {
+					return e.team == 0;
+				});
+			},
+			blueTeamList() {
+				let data = this.$store.state.PlayerData as playerInfo[];
+				return data.filter((e: playerInfo) => {
+					return e.team == 1;
+				});
+			},
+			selectedPlayer() {
+				return this.$store.state.PlayerData[this.$store.state.selectedIndex];
+			},
+			healthBar() {
+				let player = this.$store.state.PlayerData[this.$store.state.selectedIndex];
+				
+				return {
+					width: `${player.health}%`
+				};
+			}
+		}),
+	});
 </script>
 
-<style scoped>
+<style>
 .container{display:grid;height:100vh;grid-template-columns:538px auto 538px;grid-template-rows:1fr 1fr;text-align:center;}
 
 .team{
@@ -113,15 +121,7 @@ export default defineComponent({
 }
 .team--red .team__logo{grid-column:1;}
 
-.team__players {
-	display: grid;
-	grid-row:3;
-	grid-template-columns: repeat(1,1fr);
-	grid-template-rows: repeat(5,1fr);
-	row-gap: 8px;
-	height: 150px;
-	width: 402px;
-}
+.team__players{display:grid;grid-row:3;grid-template-columns:repeat(1,1fr);grid-template-rows:repeat(5,1fr);height:150px;row-gap:8px;width:402px;}
 .team__players--blue{grid-column:2;}
 .team__players--red{justify-self:end;}
 
@@ -148,28 +148,50 @@ export default defineComponent({
 
 #minimap{grid-row-start:-1;}
 
-.playerBar {
-	align-items: end;
-	display: flex;
-	grid-column: 2;
-	grid-row: -1;
-	justify-content: center;
-	margin-bottom: 1em;
-}
+.playerBar{align-items:end;display:flex;grid-column:2;grid-row:-1;justify-content:center;margin-bottom:1em;}
 .playerBar_wrapper {
 	background: transparent url('../assets/dln-ui-player-bar.png') no-repeat scroll 0 0;
 	color: #fff;
 	display: grid;
-	grid-template-columns: 42px 8px 40px 3px 83px 15px 140px 13px 84px 3px 40px 8px auto;
-	grid-template-rows: 7px 23px 6px 7px 8px 30px 8px 3px;
+	grid-template-columns: 42px 8px 40px 3px 83px 15px 140px 18px 74px 3px 40px 8px auto;
+	grid-template-rows: 8.5px 21px 6px 7px 8px 30px 8px 3px;
 	height: 93px;
 	width: 523px;
 }
 .playerBar_wrapper > div{align-items:center;display:flex;justify-content:center;}
 .playerBar__dashes{grid-column:3 / span 3;grid-row:2;}
 .playerBar__score{font-size:24px;font-weight:700;grid-column:7;grid-row:2 / span 3;}
-.playerBar__health{grid-column:9 / span 2;grid-row:2;}
+.playerBar__health{grid-column:9 / span 3;grid-row:2;justify-content:start!important;}
 .playerBar__kills{font-size:24px;font-weight:700;grid-column:2 / span 2;grid-row:4 / span 3;}
 .playerBar__deaths{font-size:24px;font-weight:700;grid-column:11 / span 2;grid-row:4 / span 3;}
 .playerBar__name{font-size:30px;font-weight:800;grid-column:4 / span 6;grid-row:6 / span 2;}
+
+.healthBar{
+	display:block;
+	position:relative;
+	width:100%;
+	height:21px;
+}
+.healthBar .healthBar__life{
+	background-color: rgba(255,255,255,0.6);
+	display:block;
+	height:100%;
+	position:relative;
+	transform: skew(20deg);
+	transition: width 0.2s ease-in-out;
+	z-index:5;
+}
+.healthBar .healthBar__track{
+	background-color:rgba(0,0,0,0.2);
+	bottom:-1px;
+	left:-1px;
+	position:absolute;
+	right:-1px;
+	transform: skew(20deg);
+	top:-1px;
+	z-index:1;
+}
+.healthBar:hover .healthBar__life{
+	width:33%!important;
+}
 </style>
