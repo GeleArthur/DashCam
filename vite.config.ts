a23817e8b3425624db481a11c6eef3eda556fd26
Arthur <geleminecrafter+git@gmail.com>
2022-11-06 00:18:52 +0100

    Fixed not using base path as origin

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path' // Errors are incorrect. WARNING dont install @types/nod
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:"./",
 
   resolve: {
     alias: {
