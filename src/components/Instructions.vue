9892821196bb457eb44a79af673085164ac60fed
Arthur <geleminecrafter+git@gmail.com>
2023-03-11 16:51:37 +0100

    Converted instructions to compostion

--- src/components/Instructions.vue
+++ src/components/Instructions.vue
@@ -2,7 +2,7 @@
   <div class="modal">
     <div class="modal_wrapper">
       <div class="modal_content">
-        <h1>DashCam {{latestHyperBashVersion}} </h1>
+        <h1>DashCam {{ latestHyperBashVersion }} </h1>
         <p>Welcome, this is a HyperDash overlay made by <b>GeleArthur</b> &amp; <b>Styledev</b></p>
         <h2>How to use</h2>
         <ol>
@@ -21,23 +21,51 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { latestHyperBashVersion } from "@/Util/ConstVars";
-import { defineComponent } from "vue";
-export default defineComponent({
-  computed: {
-    latestHyperBashVersion() {
-      return latestHyperBashVersion
-    }
-  }
-});
 </script>
 
 <style scoped lang="css">
-  .modal{align-items:center;bottom:0;display:flex;justify-content:center;left:0;position:fixed;right:0;top:0;z-index:5;}
-  .modal h1{margin-top:0;}
-  .modal:before{background-color:rgba(0,0,0,0.3);bottom:0;content:'';left:0;position:absolute;right:0;top:0;z-index:1;}
-  .modal_wrapper{height:90vh;overflow:scroll;position:relative;width:800px;z-index:1;}
-  .modal_content{background-color:#fff;padding:2em;}
-  .modal li+li{margin-top:1em;}
-</style>
+.modal {
+  align-items: center;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: 5;
+}
+
+.modal h1 {
+  margin-top: 0;
+}
+
+.modal:before {
+  background-color: rgba(0, 0, 0, 0.3);
+  bottom: 0;
+  content: '';
+  left: 0;
+  position: absolute;
+  right: 0;
+  top: 0;
+  z-index: 1;
+}
+
+.modal_wrapper {
+  height: 90vh;
+  overflow: scroll;
+  position: relative;
+  width: 800px;
+  z-index: 1;
+}
+
+.modal_content {
+  background-color: #fff;
+  padding: 2em;
+}
+
+.modal li+li {
+  margin-top: 1em;
+}</style>
